4c1554127d90ae2449d09e34a0f4f31aa8859210
unknown <sangabiwork@gmail.com>
2024-03-30 21:03:31 +0330

    first commit

---
+++ src/context/WalletConnectProvider.jsx
@@ -0,0 +1,55 @@
+import PropTypes from "prop-types";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+
+import { http, createConfig, WagmiProvider } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { walletConnect, injected, coinbaseWallet } from "wagmi/connectors";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const projectId = import.meta.env.VITE_PROJECT_ID;
+if (!projectId) throw new Error("Project ID is undefined");
+
+const metadata = {
+  name: "web3modal",
+  description: "web3modal example for me !",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, sepolia];
+
+const config = createConfig({
+  chains,
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+  connectors: [
+    walletConnect({ projectId, metadata, showQrModal: false }),
+    injected({ shimDisconnect: true }),
+    coinbaseWallet({
+      appName: metadata.name,
+      appLogoUrl: metadata.icons[0],
+    }),
+  ],
+});
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+});
+
+export default function CustomConnectorProvider({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+CustomConnectorProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
